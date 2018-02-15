12b598c66455998c139d1139ff77efbefb7cd004
AoH95 <valentin.leymonie.es@gmail.com>
2018-02-15 16:02:41 +0100

    add image sport

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -3,9 +3,12 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\CategoryRepository")
+ * @Vich\Uploadable
  * @ORM\Table(name="ffroller_category")
  */
 class Category
@@ -16,6 +19,20 @@ class Category
      * @ORM\Column(type="integer")
      */
     private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255)
+     */
+    private $image;
+
+    /**
+     * @var File
+     *
+     * @Vich\UploadableField(mapping="category", fileNameProperty="image")
+     */
+    private $imageFile;
     
     /**
      * @var string
@@ -430,6 +447,34 @@ class Category
         public function getDeletedat()
         {
             return $this->deletedat;
+        }
+
+        public function setImageFile(File $image = null)
+        {
+            $this->imageFile = $image;
+    
+            // VERY IMPORTANT:
+            // It is required that at least one field changes if you are using Doctrine,
+            // otherwise the event listeners won't be called and the file is lost
+            if ($image) {
+                // if 'updatedAt' is not defined in your entity, use another property
+                $this->updatedat = new \DateTime('now');
+            }
+        }
+    
+        public function getImageFile()
+        {
+            return $this->imageFile;
+        }
+    
+        public function setImage($image)
+        {
+            $this->image = $image;
+        }
+    
+        public function getImage()
+        {
+            return $this->image;
         }
 
         public function __toString(){
